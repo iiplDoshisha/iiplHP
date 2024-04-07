5840fdc2fbd569dad9dbadde016a53b5108f8d51
Ryosuke <ryosuke.kamada.1998@gmail.com>
2024-04-07 18:43:09 +0900

    fix: helloに変更

--- frontend/web-front/src/app/page.tsx
+++ frontend/web-front/src/app/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const pages = () => {
-  return (
-    <div>Heloo world</div>  
-  )
-}
+  return <div>Heloo world</div>;
+};
 
-export default pages
+export default pages;
