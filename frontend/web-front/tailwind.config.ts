2d51267cda7a60c97ba3f26f6f17b58a8142c9b0
Iori Takeuchi <iorizhunei2@gmail.com>
2024-04-19 17:47:27 +0900

    feat:Titleの実装

--- frontend/web-front/tailwind.config.ts
+++ frontend/web-front/tailwind.config.ts
@@ -14,6 +14,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'title-gray':'#595959'
+      },
+      fontFamily: {
+        title:['Impact']
+      }
     },
   },
   plugins: [],
