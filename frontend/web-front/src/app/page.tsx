e084d43af34cbbacf41c2818df2e548fdf282e0f
Iori Takeuchi <iorizhunei2@gmail.com>
2024-04-23 14:09:37 +0900

    feat:最終更新日の実装

--- frontend/web-front/src/app/page.tsx
+++ frontend/web-front/src/app/page.tsx
@@ -1,7 +1,15 @@
 import Header from "@/components/header/Header";
+import Title from "@/components/title/Title";
+import DateUpdated from "@/components/date_updated/DateUpdated";
+import Footer from "@/components/footer/Footer";
 
 export default function Home() {
   return (
-    <div>hello</div>
+    <div className="flex-col h-screen">
+        <Header/>
+            <Title text='Research'/>
+            <DateUpdated/>
+        <Footer/>
+    </div>
   );
 }
